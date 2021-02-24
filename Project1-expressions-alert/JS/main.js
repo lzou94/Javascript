0d27b4be56ae647ab765582c8c025cd244dc5cf0
lzou94 <lisazou94@gmail.com>
2021-02-23 19:23:03 -0500

    step 59 assignment

---
+++ Project1-expressions-alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("Welcome to my javascript homework")
+var A="We will be doing a simple math expression", B =" 3+3="
+var A = A.fontcolor("blue");
+var B = B.fontcolor("red");
+document.write(A
+    + B)
+document.write(3+3)
+
+//This will make a pop up window and then display text. I couldnt figure out how to concatenate and write a math expression in one document.write line
