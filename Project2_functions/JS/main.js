fb87016a8eda48de09dd5ef04b87f6a9091d7c2f
lzou94 <lisazou94@gmail.com>
2021-03-06 13:21:12 -0500

    step 68

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -3,3 +3,9 @@ function My_First_Function() { //Defines and names the function
     var result = str.fontcolor("green"); //Defines the text color of said string
     document.getElementById("Green_Text") .innerHTML = result; //ID name of the function, use this to call
 }
+
+function myFunction() {
+    var sentence = "I am learning"; 
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate") .innerHTML = sentence; //Changes HTML element. on click, changes sentence and then adds the second part.
+}
