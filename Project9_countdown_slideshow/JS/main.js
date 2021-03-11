3967f4a08fb7ccba65557d62fd2f12762a5832d0
lzou94 <lisazou94@gmail.com>
2021-03-11 10:08:03 -0800

    revision

--- Project9_countdown_slideshow/JS/main.js
+++ Project9_countdown_slideshow/JS/main.js
@@ -1,3 +1,18 @@
+function countdown() {
+    var seconds = document.getElementById("seconds") .value;
+
+    function tick() {
+        seconds = seconds -1;
+        timer .innerHTML = seconds;
+        setTimeout(tick, 1000);
+    if(seconds == -1) {
+        alert("Time's up!");
+    }
+
+        }
+    tick();
+}
+
 var slideIndex = 1;
 showSlides(slideIndex); 
 //Starts the slides and names their positioning
